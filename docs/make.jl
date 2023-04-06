92b310459151ca9f6912fe55faae9892d0b07b5f
just-ary27 <aryan_r@ch.iitr.ac.in>
2023-04-06 22:51:05 +0530

    test documentation build

--- docs/make.jl
+++ docs/make.jl
@@ -1,26 +1,27 @@
-# using Documenter
-# using CaseStudy
+using Documenter
+using CaseStudy
 
-# makedocs(
-#     sitename = "CaseStudy.jl",
-#     format = Documenter.HTML(
-#         assets = ["assets/favicon.ico"]
-#     ),
-#     modules = [CaseStudy],
-#     pages = [
-#         "Home" => "index.md",
-#         "Manual" => [
-#             "Guide" => "manual/guide.md",
-#             "Examples" => "manual/examples.md",
-#         ],
-#         "Library" => [
-#             "Core" => "library/core.md",
-#             "Utils" => "library/utils.md",
-#         ],
-#         "License" => "license.md"
-#     ]
-# )
+makedocs(
+    sitename = "CaseStudy.jl",
+    format = Documenter.HTML(
+        assets = ["assets/favicon.ico"]
+    ),
+    modules = [CaseStudy],
+    pages = [
+        "Home" => "index.md",
+        "Manual" => [
+            "Guide" => "manual/guide.md",
+            "Examples" => "manual/examples.md",
+        ],
+        "Library" => [
+            "Core" => "library/core.md",
+            "Utils" => "library/utils.md",
+        ],
+        "License" => "license.md"
+    ]
+)
 
-# deploydocs(
-#     repo = "github.com/just-ary27/CaseStudy.git",
-# )
+deploydocs(
+    repo = "github.com/just-ary27/CaseStudy.git",
+    versions = nothing,
+)
